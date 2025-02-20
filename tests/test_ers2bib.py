0cb6a3e0c061c84da698c13a1d6c09303cc9501f
dylan-turner25 <dylan.turner25@uga.edu>
2025-02-20 06:29:36 -0500

    .

--- tests/test_ers2bib.py
+++ tests/test_ers2bib.py
@@ -1,10 +1,11 @@
 import pytest
 
+def test_gen_cite_key():
+    """
+    Test the gen_cite_key function.
+    """
+    from pyciters.ers2bib import gen_cite_key
+    result = gen_cite_key(['Dylan Turner', 'Francis Tsiboe'], '2023-12-19')
+    assert result == 'Turner_2023'
 
-def test_result_true():
-    """
-    Test that the result is true.
-    """
-    result = True
-    assert result == True
 

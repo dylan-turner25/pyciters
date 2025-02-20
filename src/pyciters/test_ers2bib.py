3f478987f92b037ca286f08f5711017d06075561
dylan-turner25 <dylan.turner25@uga.edu>
2025-02-20 06:52:58 -0500

    .

--- src/pyciters/test_ers2bib.py
+++ src/pyciters/test_ers2bib.py
@@ -1,10 +1,9 @@
 import pytest
-
+from src.pyciters.ers2bib import gen_cite_key
 def test_gen_cite_key():
     """
     Test the gen_cite_key function.
     """
-    from pyciters.ers2bib import gen_cite_key
     result = gen_cite_key(['Dylan Turner', 'Francis Tsiboe'], '2023-12-19')
     assert result == 'Turner_2023'
 
